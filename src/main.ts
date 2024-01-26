b14581b09aa8d1f45b08a535d5869c3f7d71e7c5
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2024-01-25 22:34:54 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable Cors
-  app.use(cors())
+  // app.use(cors())
   app.enableCors({
     origin: "http://localhost:3000",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
